c295a016958160bda855703a3935123141aa5330
JAdshead <jonny.adshead@gmail.com>
2013-12-16 13:48:22 +0000

    home ajax page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-2.0.3.js
 //= require_tree .
 //= require underscore.js
 //= require modernizr.custom.40054.js
 //= require typeahead.min.js
 //= require main.js.coffee
+//= require video.js
+//= require pagination.js
 
