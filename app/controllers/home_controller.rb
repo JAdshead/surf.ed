7dabac35d15630dd5fd757791ce81c60ebeda2cc
JAdshead <jonny.adshead@gmail.com>
2013-12-18 17:46:04 +0000

    some more styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @users = User.paginate(page: params[:user_page], per_page: 5).order("score DESC").all
-    @topics = Topic.paginate(page: params[:topic_page], per_page: 5).order("score DESC").where(answered: true)
-    @unanswered = Topic.paginate(page: params[:unanswered_page], per_page: 5).order("score DESC").where(answered: false)
+    @users = User.paginate(page: params[:user_page], per_page: 6).order("score DESC").all
+    @topics = Topic.paginate(page: params[:topic_page], per_page: 6).order("score DESC").where(answered: true)
+    @unanswered = Topic.paginate(page: params[:unanswered_page], per_page: 6).order("score DESC").where(answered: false)
   end
 
 end
