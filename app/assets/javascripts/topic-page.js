423fd903a502a9f044ddc1ef3a18bc44c6dc4942
JAdshead <jonny.adshead@gmail.com>
2013-12-19 17:12:01 +0000

    form succeded but not quite right..

--- app/assets/javascripts/topic-page.js
+++ app/assets/javascripts/topic-page.js
@@ -1,8 +1,18 @@
-// $(function(){
-//   $(document).on("click",".answer-button-topic", function() {
-//     $(".answer-button-topic").html("Loading . . ")
-//     $.get(this.href, null, null, "script")
-//     return false;
-//   })
-// })
+$(function(){
+  $("#new_answer").on("submit", function(e) {
+    e.preventDefault(); 
+    answer = $("#answer_answer").val();
+    topic_id = $("#topic_id").val();
+    $.ajax({
+      url: "/answers",
+      method: "POST",
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+      data: {topic_id: topic_id, answer:{answer: answer}},
+      success: function(data){
+        $("#form-submited").html("Your answer has been submited")
+      }
 
+    })
+    return false;
+  })
+})
