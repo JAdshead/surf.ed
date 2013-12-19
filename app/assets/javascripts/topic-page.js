56a2dfb54e578d52e71c2bc4ba58019ceb98d1a0
JAdshead <jonny.adshead@gmail.com>
2013-12-19 16:20:59 +0000

    failing ajax

---
+++ app/assets/javascripts/topic-page.js
@@ -0,0 +1,8 @@
+// $(function(){
+//   $(document).on("click",".answer-button-topic", function() {
+//     $(".answer-button-topic").html("Loading . . ")
+//     $.get(this.href, null, null, "script")
+//     return false;
+//   })
+// })
+
